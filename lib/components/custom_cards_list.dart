18fa100836b3c054355255a45afa536500898299
Gabriel S. Souza <gabriel12saraiva@gmail.com>
2022-04-29 23:48:43 -0300

    solves the bug that allowed turning over more than two cards when performing double taps on the card list

--- lib/components/custom_cards_list.dart
+++ lib/components/custom_cards_list.dart
@@ -4,6 +4,7 @@ import 'package:memory_game/components/custom_info_banner.dart';
 import 'package:memory_game/components/custom_modal.dart';
 import 'package:memory_game/controllers/game_controller.dart';
 import 'package:memory_game/models/game_model.dart';
+import 'package:memory_game/tools/single_touch_gesture_recognizer.dart';
 
 import '../tools/page_modal.dart';
 
@@ -93,127 +94,129 @@ class _CustomCardsListState extends State<CustomCardsList> {
         heightFraction = constraints.maxWidth;
       }
 
-      return GridView.count(
-        padding: EdgeInsets.only(left: widget.padding, right: widget.padding),
-        shrinkWrap: true,
-        physics: const ScrollPhysics(parent: NeverScrollableScrollPhysics()),
-        crossAxisCount: 4,
-        mainAxisSpacing: widget.padding,
-        crossAxisSpacing: widget.padding,
-        childAspectRatio: widthFraction / heightFraction,
-        children: List.generate(_gameModel.numberOfPairs * 2, (index) {
-          return CustomCard(
-            pathImage: _shuffledImagePaths![index],
-            index: index,
-            isMatched: gameController.matchedCards.contains(index),
-            gameStatus: gameStatus,
-            onTap: (path, index) {
-              if (flippedCards.isEmpty) {
-                flippedCards.add({'path': path, 'index': index});
-              } else if (flippedCards.length == 1) {
-                flippedCards.add({'path': path, 'index': index});
-                gameController.validateMatch(flippedCards);
-                flippedCards = [];
-              }
-
-              if (gameController.matchedCards.length ==
-                  gameController.numberOfCards) {
-                if (gameController.isMultplayer) {
-                  if (gameController.score > gameController.score2) {
-                    Navigator.of(context)
-                        .push(ModalPage(
-                      myBarrierColor: Colors.black.withOpacity(0.5),
-                      builder: (context) {
-                        return CustomModal(
-                          title: '${_gameModel.playerNames[0]} venceu!!!',
-                          child: Image.asset(
-                            'assets/images/trophy.png',
-                            fit: BoxFit.cover,
-                          ),
-                          onTap: () {
-                            Navigator.of(context).pop();
-                          },
-                        );
-                      },
-                    ))
-                        .then((value) {
-                      _shuffledImagePaths =
-                          _shuffleImagePaths(_gameModel.getImagesPath());
-                      gameController.notifier!.value++;
-                      _showInfoBanner(gameController, context);
-                    });
-                  } else if (gameController.score < gameController.score2) {
-                    Navigator.of(context)
-                        .push(ModalPage(
-                      myBarrierColor: Colors.black.withOpacity(0.5),
-                      builder: (context) {
-                        return CustomModal(
-                          title: '${_gameModel.playerNames[1]} venceu!!!',
-                          child: Image.asset(
-                            'assets/images/trophy.png',
-                            fit: BoxFit.cover,
-                          ),
-                          onTap: () {
-                            Navigator.of(context).pop();
-                          },
-                        );
-                      },
-                    ))
-                        .then((value) {
-                      _shuffledImagePaths =
-                          _shuffleImagePaths(_gameModel.getImagesPath());
-                      gameController.notifier!.value++;
-                      _showInfoBanner(gameController, context);
-                    });
+      return SingleTouchRecognizerWidget(
+        child: GridView.count(
+          padding: EdgeInsets.only(left: widget.padding, right: widget.padding),
+          shrinkWrap: true,
+          physics: const ScrollPhysics(parent: NeverScrollableScrollPhysics()),
+          crossAxisCount: 4,
+          mainAxisSpacing: widget.padding,
+          crossAxisSpacing: widget.padding,
+          childAspectRatio: widthFraction / heightFraction,
+          children: List.generate(_gameModel.numberOfPairs * 2, (index) {
+            return CustomCard(
+              pathImage: _shuffledImagePaths![index],
+              index: index,
+              isMatched: gameController.matchedCards.contains(index),
+              gameStatus: gameStatus,
+              onTap: (path, index) {
+                if (flippedCards.isEmpty) {
+                  flippedCards.add({'path': path, 'index': index});
+                } else if (flippedCards.length == 1) {
+                  flippedCards.add({'path': path, 'index': index});
+                  gameController.validateMatch(flippedCards);
+                  flippedCards = [];
+                }
+      
+                if (gameController.matchedCards.length ==
+                    gameController.numberOfCards) {
+                  if (gameController.isMultplayer) {
+                    if (gameController.score > gameController.score2) {
+                      Navigator.of(context)
+                          .push(ModalPage(
+                        myBarrierColor: Colors.black.withOpacity(0.5),
+                        builder: (context) {
+                          return CustomModal(
+                            title: '${_gameModel.playerNames[0]} venceu!!!',
+                            child: Image.asset(
+                              'assets/images/trophy.png',
+                              fit: BoxFit.cover,
+                            ),
+                            onTap: () {
+                              Navigator.of(context).pop();
+                            },
+                          );
+                        },
+                      ))
+                          .then((value) {
+                        _shuffledImagePaths =
+                            _shuffleImagePaths(_gameModel.getImagesPath());
+                        gameController.notifier!.value++;
+                        _showInfoBanner(gameController, context);
+                      });
+                    } else if (gameController.score < gameController.score2) {
+                      Navigator.of(context)
+                          .push(ModalPage(
+                        myBarrierColor: Colors.black.withOpacity(0.5),
+                        builder: (context) {
+                          return CustomModal(
+                            title: '${_gameModel.playerNames[1]} venceu!!!',
+                            child: Image.asset(
+                              'assets/images/trophy.png',
+                              fit: BoxFit.cover,
+                            ),
+                            onTap: () {
+                              Navigator.of(context).pop();
+                            },
+                          );
+                        },
+                      ))
+                          .then((value) {
+                        _shuffledImagePaths =
+                            _shuffleImagePaths(_gameModel.getImagesPath());
+                        gameController.notifier!.value++;
+                        _showInfoBanner(gameController, context);
+                      });
+                    } else {
+                      Future.delayed(const Duration(milliseconds: 1000), () {
+                        _shuffledImagePaths =
+                            _shuffleImagePaths(_gameModel.getImagesPath());
+                        gameController.notifier!.value++;
+                        _showInfoBanner(gameController, context);
+                      });
+                    }
                   } else {
-                    Future.delayed(const Duration(milliseconds: 1000), () {
+                    Navigator.of(context)
+                        .push(ModalPage(
+                      myBarrierColor: Colors.black.withOpacity(0.5),
+                      builder: (context) {
+                        return CustomModal(
+                          title: gameController.lastGameWasRecord
+                              ? 'UAAAU!'
+                              : 'YEEEEEEAH!',
+                          subtitle: gameController.lastGameWasRecord
+                              ? 'Novo record:'
+                              : 'Tempo:',
+                          showConffetti: gameController.lastGameWasRecord,
+                          secondSubtitle: gameController.time,
+                          child: Image.asset(
+                            gameController.lastGameWasRecord
+                                ? 'assets/images/trophy-record.png'
+                                : 'assets/images/trophy.png',
+                            fit: BoxFit.cover,
+                          ),
+                          onTap: () {
+                            Navigator.of(context).pop();
+                          },
+                        );
+                      },
+                    ))
+                        .then((value) {
                       _shuffledImagePaths =
                           _shuffleImagePaths(_gameModel.getImagesPath());
                       gameController.notifier!.value++;
-                      _showInfoBanner(gameController, context);
                     });
                   }
-                } else {
-                  Navigator.of(context)
-                      .push(ModalPage(
-                    myBarrierColor: Colors.black.withOpacity(0.5),
-                    builder: (context) {
-                      return CustomModal(
-                        title: gameController.lastGameWasRecord
-                            ? 'UAAAU!'
-                            : 'YEEEEEEAH!',
-                        subtitle: gameController.lastGameWasRecord
-                            ? 'Novo record:'
-                            : 'Tempo:',
-                        showConffetti: gameController.lastGameWasRecord,
-                        secondSubtitle: gameController.time,
-                        child: Image.asset(
-                          gameController.lastGameWasRecord
-                              ? 'assets/images/trophy-record.png'
-                              : 'assets/images/trophy.png',
-                          fit: BoxFit.cover,
-                        ),
-                        onTap: () {
-                          Navigator.of(context).pop();
-                        },
-                      );
-                    },
-                  ))
-                      .then((value) {
-                    _shuffledImagePaths =
-                        _shuffleImagePaths(_gameModel.getImagesPath());
-                    gameController.notifier!.value++;
-                  });
                 }
-              }
-            },
-            updateGameStatus: (status) {
-              setState(() {
-                gameStatus = status;
-              });
-            },
-          );
-        }),
+              },
+              updateGameStatus: (status) {
+                setState(() {
+                  gameStatus = status;
+                });
+              },
+            );
+          }),
+        ),
       );
     });
   }
---
+++ lib/tools/single_touch_gesture_recognizer.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class SingleTouchRecognizerWidget extends StatelessWidget {
+  final Widget child;
+  const SingleTouchRecognizerWidget({Key? key, required this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RawGestureDetector(
+      gestures: <Type, GestureRecognizerFactory>{
+        _SingleTouchRecognizer: GestureRecognizerFactoryWithHandlers<_SingleTouchRecognizer>(
+          () => _SingleTouchRecognizer(),
+          (_SingleTouchRecognizer instance) {},
+        ),
+      },
+      child: child,
+    );
+  }
+}
+
+class _SingleTouchRecognizer extends OneSequenceGestureRecognizer {
+  
+  int _p = 0;
+
+  @override
+  void addAllowedPointer(PointerDownEvent event) {
+    startTrackingPointer(event.pointer, event.transform);
+    //first register the current pointer so that related events will be handled by this recognizer
+    // startTrackingPointer(event.pointer);
+    //ignore event if another event is already in progress
+    if (_p == 0) {
+      resolve(GestureDisposition.rejected);
+      _p = event.pointer;
+    } else {
+      resolve(GestureDisposition.accepted);
+    }
+  }
+
+  @override
+  String get debugDescription => '_SingleTouchRecognizer';
+
+  @override
+  void didStopTrackingLastPointer(int pointer) {}
+
+  @override
+  void handleEvent(PointerEvent event) {
+    if (!event.down && event.pointer == _p) {
+      _p = 0;
+    }
+  }
+}
